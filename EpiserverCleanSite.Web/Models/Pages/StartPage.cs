c69d0d83d2d9d7f91b05cd9089eeb0bbd9202d8b
duskjet <duskjet@gmail.com>
2017-03-13 18:36:01 +0200

    add start page

---
+++ EpiserverCleanSite.Web/Models/Pages/StartPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EpiserverCleanSite.Web.Models.Pages
+{
+    [ContentType(DisplayName = "StartPage", GUID = "85a2ec16-d4e9-46d5-8c0b-f3ca28d32ee7", Description = "")]
+    public class StartPage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
